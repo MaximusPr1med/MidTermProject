91b87e4479cf3d2b58b13f0ecde4070ac7c56d30
MaximusPr1med <maxshoemaker2@gmail.com>
2024-10-06 03:26:28 -0400

    commit

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -36,7 +36,7 @@ public class HelloWorldTest {
         method.setAccessible(true);
         String fact = (String) method.invoke(null);
 
-        // Ensure the method returns a non-null and non-empty string aaaa
+        // Ensure the method returns a non-null and non-empty string a
         assertNotNull(fact);
         assertFalse(fact.isEmpty());
     }
